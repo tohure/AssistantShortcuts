03851197a2f213c84709d553553d79f58d64242a
Carlo Huaman Torres <cr.htorres@gmail.com>
2021-06-13 14:45:26 -0500

    remove safe args, call by deep link

--- app/src/main/java/io/tohure/shortcuts/MenuFragment.kt
+++ app/src/main/java/io/tohure/shortcuts/MenuFragment.kt
@@ -1,10 +1,10 @@
 package io.tohure.shortcuts
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import io.tohure.shortcuts.databinding.FragmentMenuBinding
@@ -24,36 +24,21 @@ class MenuFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        validateShortcut()
         setupClicks()
-    }
-
-    private fun validateShortcut() {
-        val action = activity?.intent?.action ?: ""
-
-        when (action) {
-            getString(R.string.action_flow_one) -> {
-                val action1 = MenuFragmentDirections.actionMenuFragmentToFlowOneFragment()
-                findNavController().navigate(R.id.flowOneFragment)
-            }
-        }
     }
 
     private fun setupClicks() {
 
         binding.btnNote.setOnClickListener {
-            val action = MenuFragmentDirections.actionMenuFragmentToFlowOneFragment()
-            findNavController().navigate(action)
+            findNavController().navigate(R.id.flowOneFragment)
         }
 
         binding.btnNews.setOnClickListener {
-            val action = MenuFragmentDirections.actionMenuFragmentToFlowTwoFragment()
-            findNavController().navigate(action)
+            findNavController().navigate(R.id.flowTwoFragment)
         }
 
         binding.btnSupport.setOnClickListener {
-            val action = MenuFragmentDirections.actionMenuFragmentToFlowThreeFragment()
-            findNavController().navigate(action)
+            findNavController().navigate(R.id.flowThreeFragment)
         }
 
     }
