acae75b82d2b357222fef4ed1f07a96e18132305
Carlo Huaman Torres <cr.htorres@gmail.com>
2021-06-14 00:25:29 -0500

    add first capability

--- app/src/main/java/io/tohure/shortcuts/flowTwo/FlowTwoFragment.kt
+++ app/src/main/java/io/tohure/shortcuts/flowTwo/FlowTwoFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import io.tohure.shortcuts.databinding.FragmentFlowTwoBinding
 
@@ -26,6 +27,14 @@ class FlowTwoFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.intent?.extras?.let {
+            Toast.makeText(context, it.getString("aboutName"), Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
